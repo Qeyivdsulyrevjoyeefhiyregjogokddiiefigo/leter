4650290c5f581ce65624242ac762b9adff019430
LitRHap <73509282+LitRHap@users.noreply.github.com>
2021-09-14 20:40:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+global.owner = ['6289681378568', '6283801499848','48699530299','62813828362494','31687221333','6285240750713','6282125986924'] // Put your number here
+global.mods = ['6285693120588'] // Want some help?
+global.prems = ['6287842147162','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com', 
+  Dehan: 'https://dhnjing.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '879aaf8cc437fcb4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://dhnjing.xyz': 'LitRHap'
+}
+
+// Sticker WM
+global.packname = 'I hope you\'re fine'
+global.author = 'LitRHap'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
