5f3f2d8a24c2501ef00ac3c595c3af87a034a309
LitRHap <73509282+LitRHap@users.noreply.github.com>
2021-09-27 20:33:32 +0700

    fix api mokad

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://justnino.herokuapp.com/api/image/cosplay`
+  let res = `https://velgrynd.herokuapp.com/api/image/cosplay`
   conn.sendFile(m.chat, res, 'cosplay.jpg', 'huu suka ama cosplayer', m)
 }
 handler.help = ['cosplay']
