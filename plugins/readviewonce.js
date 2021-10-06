adafb0348529e559d9aa14d3d4cfc1629ecf9305
Ultima (≧ ∇ ≦) <81684610+ultimareall@users.noreply.github.com>
2021-10-06 08:28:36 +0700

    Add files via upload

--- plugins/readviewonce.js
+++ plugins/readviewonce.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
     if (!m.quoted) return conn.sendMessage(m.chat, 'where\'s message?', 'conversation')
-
-    await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { readViewOnce: true })
+    if (m.quoted.m.mtype !== 'viewOnceMessage') return conn.sendMessage(m.chat, 'Reply chat viewOnce')
+    conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), true, { readViewOnce: true, quoted: m }).catch(console.log)
 }
 
 handler.help = ['readviewonce']
