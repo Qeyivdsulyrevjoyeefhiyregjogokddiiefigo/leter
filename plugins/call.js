6a49e933a6e6f74cdb586cefe71f122410d41c5a
Ultima (≧ ∇ ≦) <81684610+ultimareall@users.noreply.github.com>
2021-10-09 10:46:58 +0700

    Add files via upload

--- plugins/call.js
+++ plugins/call.js
@@ -1,8 +1,9 @@
+let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix }) => {
 
-if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
+if (!text) throw `Contoh Penggunaan\n${usedPrefix}call 628xxxxxxxx`
 let nomor = text.replace(/[^0-9]/gi, '').slice(2)
-if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
+if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}call 628xxxxxxxx`
 m.reply('*_Tunggu permintaan anda sedang diproses_*')
 let anu = await fetch(`https://id.jagreward.com/member/verify-mobile/${nomor}`).then(a => a.json())
 let spcall = `Sukses mengirimkan call ke 62${nomor}`
