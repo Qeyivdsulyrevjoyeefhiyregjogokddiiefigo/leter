c4142fe3f2017f3a46ce677aa69c1ec63c623a5c
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-10-10 17:55:25 +0700

    Tch.

--- plugins/menu.js
+++ plugins/menu.js
@@ -155,7 +155,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     //Merasa keren
-conn.sendMessage(m.chat, text.trim() , 'conversation', {quoted: m, thumbnail: global.thumb, contextInfo:{externalAdReply: {title: 'Just simple WhatsApp Bot', body: `© ${conn.user.name}, sourceUrl: '', thumbnail: global.thumb2}}})
+conn.sendMessage(m.chat, text.trim() , 'conversation', {quoted: m, thumbnail: global.thumb, contextInfo:{externalAdReply: {title: 'Just simple WhatsApp Bot', body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: global.thumb2}}})
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
