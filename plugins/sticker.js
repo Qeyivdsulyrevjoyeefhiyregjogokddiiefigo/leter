e66ce3f1afb0c771da9655d3074cf7358dcb0e8e
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-10-19 12:59:45 +0700

    Awikwok
    :v

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,6 +1,10 @@
+//wm by RelldevUwU
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 let handler  = async (m, { conn, args }) => {
+  let nick = conn.contacts[m.sender].notify
+
+let gc = conn.getName(m.chat)
   let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
@@ -8,7 +12,7 @@ let handler  = async (m, { conn, args }) => {
     if (/image|video/.test(mime)) {
       let img = await q.download()
       if (!img) throw 'Foto/Video tidak ditemukan'
-      stiker = await sticker(img, false, global.packname, global.author)
+      stiker = await sticker(img, false, nick || '', `© ${conn.user.name}` || '')
     } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
