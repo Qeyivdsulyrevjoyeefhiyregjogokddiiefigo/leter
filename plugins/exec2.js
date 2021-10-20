66aa62fcb364698348ff9daa4f88207c0305b0d5
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-10-20 13:46:06 +0700

    >///<

--- plugins/exec2.js
+++ plugins/exec2.js
@@ -1,9 +1,12 @@
 let cp = require('child_process')
 let { promisify } = require('util')
-let exec = promisify(cp.exec).bind(cp)
+let fs = require('fs')
+let { MessageType } = require('@adiwajshing/baileys')
+let { performance } = require('perf_hooks')
 let handler = async (m, { conn, isOwner, command, text }) => {
-  if (global.conn.user.jid != conn.user.jid) return
-  m.reply('Executing...')
+  if (global.conn.user.jid != conn.user.jid) return /*conn.reply(m.chat, util.format(...args), m)*/
+  let fetch = require('node-fetch')
+await conn.sendMessage(m.chat, '```Executing...```', 'conversation', { thumbnail:  await (await fetch('https://i.ibb.co/H2xpv56/elaina2.jpg')).buffer(), contextInfo: { forwardingScore: '508', isForwarded: true, stanzaId: 'xxx', participant: '0@s.whatsapp.net', quotedMessage: { viewOnceMessage: { message: { imageMessage: {viewOnce: true}}}}, remoteJid: 'status@broadcast' }})
   let o
   try {
     o = await exec(command.trimStart()  + ' ' + text.trimEnd())
@@ -11,11 +14,14 @@ let handler = async (m, { conn, isOwner, command, text }) => {
     o = e
   } finally {
     let { stdout, stderr } = o
-    if (stdout.trim()) m.reply(stdout)
-    if (stderr.trim()) m.reply(stderr)
+    if (stdout.trim())  /*conn.reply(m.chat, util.format(...args), m)*/ 
+await conn.sendMessage(m.chat, stdout, 'conversation', { thumbnail:  await (await fetch('https://i.ibb.co/H2xpv56/elaina2.jpg')).buffer(), contextInfo: { forwardingScore: '508', isForwarded: true, stanzaId: 'xxx', participant: '0@s.whatsapp.net', quotedMessage: { viewOnceMessage: { message: { imageMessage: {viewOnce: true}}}}, remoteJid: 'status@broadcast' }})
+    if (stderr.trim())  /*conn.reply(m.chat, util.format(...args), m)*/ 
+await conn.sendMessage(m.chat, stderr, 'conversation', { thumbnail:  await (await fetch('https://i.ibb.co/H2xpv56/elaina2.jpg')).buffer(), contextInfo: { forwardingScore: '508', isForwarded: true, stanzaId: 'xxx', participant: '0@s.whatsapp.net', quotedMessage: { viewOnceMessage: { message: { imageMessage: {viewOnce: true}}}}, remoteJid: 'status@broadcast' }})
   }
 }
 handler.customPrefix = /^[$] /
 handler.command = new RegExp
 handler.rowner = true
 module.exports = handler
+//UwU
