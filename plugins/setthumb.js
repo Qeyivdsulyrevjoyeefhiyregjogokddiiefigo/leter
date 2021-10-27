93c1d3b5fa99209291ec386927b128e3335ffc01
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-10-27 13:38:30 +0700

    Mapp kalo emror

---
+++ plugins/setthumb.js
@@ -0,0 +1,16 @@
+//just trying, maap kalo eror
+
+let handler = async m => {
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || ''
+	if (/image/.test(mime)) {
+		let img = await q.download()
+		 await conn.downloadAndSaveMediaMessage({ message: m.message.extendedTextMessage.contextInfo.quotedMessage }, './src/thumb')
+	} else throw 'Reply imagenya'
+}
+
+handler.help = ['thumb'].map(v => 'set' + v)
+handler.command = /^set(thumb|thumbnail)$/i
+handler.owner = true
+
+module.exports = handler
