e8fd1ea6493c65e80874869e3f2342b3bd5523d4
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-10-27 14:12:47 +0700

    iy

--- plugins/setthumb.js
+++ plugins/setthumb.js
@@ -1,12 +1,11 @@
 //just trying, maap kalo eror
-
+//Reply gambar bang, jangan send gambar pake caption , nanti eror
 let handler = async m => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || ''
 	if (/image/.test(mime)) {
 		let img = await q.download()
-		 await conn.downloadAndSaveMediaMessage({ message: m.message.extendedTextMessage.contextInfo.null }, './src/thumb')
-m.reply('done')
+		 await conn.downloadAndSaveMediaMessage({ message: m.message.extendedTextMessage.contextInfo.null }, './src/thumb').(() => then m.reply('done!'))
 	} else throw 'Reply imagenya'
 }
 
