a172db02c088cdcbd8b10397948a3ccdfe5c0036
Ripp <75048902+Akkun3704@users.noreply.github.com>
2021-10-30 20:37:40 +0700

    Update twitter.js

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,19 +1,12 @@
 let fetch = require('node-fetch')
+
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/twitter', { url: args[0] }, 'APIKEY'))
-  if (res.status != 200) throw await res.text()
+  let res = await fetch(API('Velgrynd', '/api/twitter', { url: args[0] }))
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.status) throw json
-  let { name, username, caption, quality, format, size, video_url } = json.result
-  conn.sendFile(m.chat, video_url, 'file.mp4', `
-Name: ${name}
-Username: ${username}
-Caption: ${caption}
-Quality: ${quality}
-Format: ${format}
-Size: ${size}
-  `.trim(), m)
+  let { desc, HD } = json.result
+  conn.sendFile(m.chat, HD, 'file.mp4', desc, m)
 }
 handler.help = ['twitter'].map(v => v + ' <url>')
 handler.tags = ['downloader']
