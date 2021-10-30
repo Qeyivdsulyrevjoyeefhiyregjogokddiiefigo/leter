dc7e901876822607e37e51f46475419c3d63f34c
Ripp <75048902+Akkun3704@users.noreply.github.com>
2021-10-30 20:45:20 +0700

    Create mediafire.js

---
+++ plugins/mediafire.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+ if (!args[0]) throw 'Uhm..url nya mana?'
+ let res = await fetch(API('Velgrynd', '/api/mediafire', { url: args[0] }))
+ if (!res.ok) throw await res.text()
+ let json = await res.json()
+ let { title, link } = json.result
+ m.reply(JSON.stringify(json.result, null, 2))
+ conn.sendFile(m.chat, link, title, '', m)
+}
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^mediafire$/i
+
+handler.limit = true
+
+module.exports = handler
