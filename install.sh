fd001aeae47e0961b60e34bc384d22c55518cfff
HiRYN <74960675+HiRYN@users.noreply.github.com>
2021-11-02 10:51:03 +0800

    :v

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@ pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
 npm install
+npm i hmtai
+npm i booru
+npm i image-to-pdf
+npm i nhentai-node-api
+npm i acrcloud
+npm i sagiri
 npm update
