48d0ebc145de3bd83769d2c04c0af289cf2dba4f
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-11-07 06:54:56 +0700

    Update tebakkimia.js

--- plugins/tebakkimia.js
+++ plugins/tebakkimia.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 500
+let poin = 2000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkimia = conn.tebakkimia ? conn.tebakkimia : {}
     let id = m.chat
@@ -9,22 +9,19 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkimia[id][0])
         throw false
     }
-    let res = await fetch(global.API('http://zekais-api.herokuapp.com', '/tebakunsur'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (json.status != 200) throw json
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkimia.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
-Nama unsur dari lambang ${json.simbol} adalah...
-
+Nama unsur dari lambang ${json.lambang} adalah...
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}teki untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkimia[id] = [
-        await conn.reply(m.chat, caption, m),
+        await conn.sendButton(m.chat, caption, 'Kesulitan?\nklik di bawah!', 'Bantuan', '.teki', m),
         json, poin,
-        setTimeout(() => {
-            if (conn.tebakkimia[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakkimia[id][0])
+        setTimeout(async () => {
+            if (conn.tebakkimia[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.unsur}*`, 'Mau main lagi?\nklik di bawah!', 'Tebak Kimia', '.tebakkimia', conn.tebakkimia[id][0])
             delete conn.tebakkimia[id]
         }, timeout)
     ]
