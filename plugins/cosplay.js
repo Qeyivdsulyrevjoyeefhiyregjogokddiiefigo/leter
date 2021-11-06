a073d1ca0c1540cac9470be8cb9011843af6db20
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-11-07 05:10:14 +0700

    Update cosplay.js

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
   let res = `https://velgrynd.herokuapp.com/api/image/cosplay`
-  conn.sendFile(m.chat, res, 'cosplay.jpg', 'huu suka ama cosplayer', m)
+  conn.sendFile(m.chat, res, 'cosplay.jpg', 'https://www.instagram.com/reteam.id/', m)
 }
 handler.help = ['cosplay']
 handler.tags = ['anime']
