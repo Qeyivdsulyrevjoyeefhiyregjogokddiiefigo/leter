332f0c6258a2a299e5d6b0ebb7b22a68e610ac34
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-11-07 06:21:58 +0700

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -1,7 +1,6 @@
-let fetch = require('node-fetch')
-
+const fetch = require('node-fetch')
 let timeout = 120000
-let poin = 500
+let poin = 2000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
@@ -9,22 +8,20 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/asahotak.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
-${json.result.soal}
+${json.soal}
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}ao untuk bantuan
 Bonus: ${poin} XP
-`.trim()
+    `.trim()
     conn.asahotak[id] = [
-        await conn.reply(m.chat, caption, m),
+        await conn.sendButton(m.chat, caption, 'Kesulitan?\nklik di bawah!', 'Bantuan', '.ao', m),
         json, poin,
-        setTimeout(() => {
-            if (conn.asahotak[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.asahotak[id][0])
+        setTimeout(async () => {
+            if (conn.asahotak[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'Mau main lagi?\nklik di bawah!', 'Asah Otak', '.asahotak', conn.asahotak[id][0])
             delete conn.asahotak[id]
         }, timeout)
     ]
