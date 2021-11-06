b6115caebf564ccac9cc9458ada55e42184b2430
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-11-07 05:18:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,10 @@
 function handler(m) {
    let listOwner = new Array()
   for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
-  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: http://github.com/Rlxfly\nEMAIL;y: ${this.user.name}@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P ANJG;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: http://github.com/Reteam\nEMAIL;y: ${this.user.name}@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;Jgn lupa salam;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
   }
   let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: m })
-return m.reply('Chat owner *P, & sv*\n\n *Ga ada sopan santun*')
+return m.reply('Chat owner *Chat to the point*\n\n *Basa basi g di jawab*')
 }
 
 handler.help = ['owner', 'creator']
